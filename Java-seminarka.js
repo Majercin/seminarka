f82f387d1489af7eda55da6d1d3b52d11a89607e
Majercin <129790874+Majercin@users.noreply.github.com>
2023-05-18 21:40:36 +0200

    Add files via upload

--- Java-seminarka.js
+++ Java-seminarka.js
@@ -1,20 +1,22 @@
-// Set the duration of each slide in milliseconds
-const duration = 1000;
 
-// Get the carousel items and total count
-const carouselItems = document.querySelectorAll('.carousel-item');
+const duration = 5000;
+const carouselItems = document.querySelectorAll('.slider-item');
 const totalItems = carouselItems.length;
-
-// Set the initial slide and timer variables
 let slide = 0;
 let timer = null;
 
-// Define the function to switch the active slide
 function switchSlide(nextSlide) {
-    // Remove the active class from the current slide
     carouselItems[slide].classList.remove('active');
-    // Add the active class to the next slide
     carouselItems[nextSlide].classList.add('active');
-    // Update the slide variable
     slide = nextSlide;
 }
+
+function startSlideshow() {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        const nextSlide = (slide + 1) % totalItems;
+        switchSlide(nextSlide);
+        startSlideshow();
+    }, duration);
+}
+startSlideshow();
